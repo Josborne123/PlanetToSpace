39671a6c781ff8048404f092e22e9fa42525eebd
Josborne123 <jwosborne321@gmail.com>
2021-02-25 18:44:54 +0000

    backup before testing

---
+++ PlanetToSpace/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    [SerializeField] float delayTime = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem deathParticle;
+    [SerializeField] ParticleSystem successParticle;
+
+
+    AudioSource myaudiosource;
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myaudiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RespondToDebugKeys();
+    }
+
+    void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; // toggle collision
+
+        }
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (isTransitioning == true || collisionDisabled)
+        {
+            return;
+        }
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartCrashSequence()
+    {
+        deathParticle.Play();
+        isTransitioning = true;
+        myaudiosource.Stop();
+        myaudiosource.PlayOneShot(deathSound);
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delayTime);
+
+    }
+
+    void StartSuccessSequence()
+    {
+        successParticle.Play();
+        isTransitioning = true;
+        myaudiosource.Stop();
+        myaudiosource.PlayOneShot(successSound);
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", delayTime);
+    }
+ 
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+}
---
+++ PlanetToSpace/Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float rotateThrust = 400f;
+    [SerializeField] float mainThrust = 1500f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem thrusterParticle;
+
+
+    Rigidbody rb;
+    AudioSource myaudiosource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        myaudiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space) || (Input.GetKey(KeyCode.W)))
+        {
+            StartToThrust();
+        }
+        else
+        {
+            StopThrusting();
+        }
+    }
+
+    void StopThrusting()
+    {
+        myaudiosource.Stop();
+        thrusterParticle.Stop();
+    }
+
+    void StartToThrust()
+    {
+        if (!thrusterParticle.isPlaying)
+        {
+            thrusterParticle.Play();
+        }
+
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!myaudiosource.isPlaying)
+        {
+            myaudiosource.PlayOneShot(mainEngine);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotateThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // un-freezing so the physics system can take over
+    }
+}
